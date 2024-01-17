294945f9b182bdad6124362fde313e70ebf97cb1
Vita Filimonova <vitka.2001@bk.ru>
2024-01-17 12:43:36 +0300

    First commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from "./Card.module.css"
+const Card = () => {
+    return (
+        <div>
+            <div className={style.card}>
+
+            </div>
+        </div>
+    );
+};
+
+export default Card;
